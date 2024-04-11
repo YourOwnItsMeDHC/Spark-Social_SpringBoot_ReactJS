9e7cbf3e5b471dbc90a452675830d45a196b717f
YourOwnItsMeDHC <deepakchourasiya7980@gmail.com>
2024-04-11 22:57:08 +0530

    Delete a specific user - API

--- src/main/java/com/deepak/controller/UserController.java
+++ src/main/java/com/deepak/controller/UserController.java
@@ -3,10 +3,10 @@ package com.deepak.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -92,6 +92,18 @@ public class UserController {
 			}
 		}
 		return null;
+	}
+	
+	
+	@DeleteMapping("/users/{userId}")
+	public String deleteUser(@PathVariable("userId") Integer id) {
+		for(User u : users) {
+			if(u.getId() == id) {
+				users.remove(u);
+			    return "User with an id : " + id + " has been deleted successfully!!!";
+			}
+		}
+		return "User not found with an id : " + id;
 	}
 
 }
