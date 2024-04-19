cbfa2cc2f231fa3304b6d9757da32842ecb8476a
YourOwnItsMeDHC <deepakchourasiya7980@gmail.com>
2024-04-19 22:24:02 +0530

    User Service

---
+++ src/main/java/com/deepak/service/UserService.java
@@ -0,0 +1,22 @@
+ package com.deepak.service;
+
+import java.util.List;
+
+import com.deepak.model.User;
+
+public interface UserService {
+	
+	public User registerUser(User user);
+	
+	public User findUserById(Integer userId);
+	
+	public User findUserByEmail(String email);
+	
+	public User followUser(Integer user1, Integer user2);
+	
+	public User updateUser(User user);
+	
+	public List<User> searchUser(String query);
+	
+
+}
